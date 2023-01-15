a5cb60bb4a7175489715c640487f36a55e73889b
baidyanathprasad <baidyanathprasadmdb@gmail.com>
2023-01-15 22:04:00 +0530

    refactor code

--- src/main/java/com/demo/spring/resources/UserController.java
+++ src/main/java/com/demo/spring/resources/UserController.java
@@ -4,19 +4,25 @@ import com.demo.spring.api.GenericResponse;
 import com.demo.spring.api.users.UserResponse;
 import com.demo.spring.domain.UserInput;
 import com.demo.spring.exception.CustomException;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import org.springframework.web.bind.annotation.*;
-
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
 	
-	Map<String, String> users = new HashMap<>();
+	private final Map<String, String> users = new HashMap<>();
 	
 	@GetMapping
 	public GenericResponse<?> getAllUsers() {
@@ -31,8 +37,8 @@ public class UserController {
 		
 		return new GenericResponse<>("Success", userResponses);
 	}
-	
-	@PostMapping
+
+    @PostMapping
 	public GenericResponse<?> createUser(@RequestBody UserInput input) {
 		if (input == null || input.getUsername() == null) {
 			return new GenericResponse<>("Failed", CustomException.BadRequest
@@ -59,6 +65,17 @@ public class UserController {
 					.getResponse("User with ID: " + userId + " not found."));
 		}
 		return new GenericResponse<>("Success", new UserResponse(userId, username));
+	}
+
+	@DeleteMapping("/{userId}")
+	public GenericResponse<?> deleteUser(@PathVariable("userId") String userId) {
+		String username = users.get(userId);
+		if (username == null) {
+			return new GenericResponse<>("User not found", CustomException.BadRequest
+					.getResponse("User with ID: " + userId + " not found."));
+		}
+		users.remove(userId);
+		return new GenericResponse<>("UserId: {} deleted from system!", userId);
 	}
 }
 
